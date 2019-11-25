f98664da63b53774a62c3433ca9f1a373a5978cd
zkan <kan@prontomarketing.com>
2019-11-25 07:05:39 +0700

    :recycle: Separate func to check prime :bear:

--- prime_numbers/prime_numbers.go
+++ prime_numbers/prime_numbers.go
@@ -1,16 +1,24 @@
 package main
 
+func isPrime(number int) bool {
+	count := 0
+	for i := 1; i <= number; i++ {
+		if number%i == 0 {
+			count++
+		}
+	}
+	if count == 2 {
+		return true
+	}
+
+	return false
+}
+
 func GetPrimeNumbers(number int) []int {
 	var results []int
 
 	for i := 2; i <= number; i++ {
-		count := 0
-		for j := 1; j <= i; j++ {
-			if i%j == 0 {
-				count++
-			}
-		}
-		if count == 2 {
+		if isPrime(i) {
 			results = append(results, i)
 		}
 	}
