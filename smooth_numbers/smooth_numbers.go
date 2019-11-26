99b4b16633963a3fdd1d1449d9b819f03f246105
zkan <kan@prontomarketing.com>
2019-11-26 14:38:51 +0700

    :sparkles: Add func to check if number is prime :bear:

--- smooth_numbers/smooth_numbers.go
+++ smooth_numbers/smooth_numbers.go
@@ -1,5 +1,18 @@
 package main
 
+func isPrime(number int) bool {
+	count := 0
+	for i := 2; i <= number; i++ {
+		if number%i == 0 {
+			count++
+		}
+	}
+	if count == 1 {
+		return true
+	}
+	return false
+}
+
 func GetSmoothNumber(number int) string {
 	if number == 16 {
 		return "power of 2"
--- smooth_numbers/smooth_numbers_test.go
+++ smooth_numbers/smooth_numbers_test.go
@@ -2,6 +2,26 @@ package main
 
 import "testing"
 
+func TestIsPrime(t *testing.T) {
+	t.Run("Input2ShouldGetTrue", func(t *testing.T) {
+		expected := true
+		result := isPrime(2)
+
+		if result != expected {
+			t.Errorf("It should get %t but got %t", expected, result)
+		}
+	})
+
+	t.Run("Input13ShouldGetTrue", func(t *testing.T) {
+		expected := true
+		result := isPrime(13)
+
+		if result != expected {
+			t.Errorf("It should get %t but got %t", expected, result)
+		}
+	})
+}
+
 func TestGetSmoothNumbers(t *testing.T) {
 	t.Run("Input16ShouldGetPowerOf2", func(t *testing.T) {
 		expected := "power of 2"
